d91380fa3b92e7e8594e2a99d8ef0a73c7574f4e
Abyan <harlem.minion09@gmail.com>
2019-10-02 07:16:40 +0700

    Membuat aplikasi tema fitness

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import polinema.ac.id.starterchapter05.activites.DynamicActivity;
 import polinema.ac.id.starterchapter05.activites.StaticActivity;
+import polinema.ac.id.starterchapter05.activites.TugasFragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handlerClickPraktikumFragment(View view) {
+        Intent intent = new Intent(this, TugasFragmentActivity.class);
+        startActivity(intent);
     }
 }
